ac39e410cc1f0ade9e74bacee88865f998f77711
cha121101 <cgamboa422@gmail.com>
2024-07-02 22:57:44 +0800

    fix some bug on footer

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -12,7 +12,7 @@ export default function page() {
   return (
     <>
         <Sidebar />
-        <h2 className='text-center py-4 font-bold text-3xl dark:text-teal-300 text-gray-600'>Projects</h2>
+        <h2 className='text-center py-4 font-bold text-3xl dark:text-teal-300 text-gray-600 mb-10'>Projects</h2>
         <div className="flex flex-wrap justify-center max-sm:pb-10 sm:pb-10 md:pb-20 lg:pb-32">
             <div className="p-4 max-w-sm animate-fade-up">
                 <div className="flex rounded-lg h-full dark:bg-gray-600 bg-white border py-52 dark:border-gray-700 p-8 flex-col">
--- src/app/service/page.js
+++ src/app/service/page.js
@@ -87,7 +87,7 @@ export default function page() {
             </div>
         </div>
       <Footer />
-    <Footer />
+
 
     </>
   );
